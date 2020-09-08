36498d6ea40527d989294b5b3aa68901cdeea077
Luis Santos <ls@MacBook-Pro-LS.local>
2020-09-08 09:21:17 -0600

    Cambio a materialize e implementación de SPA

---
+++ js/uiLoad.js
@@ -0,0 +1,34 @@
+"use strict";
+
+function navigatePage(load) {
+    let current = ["#main", "#help"];
+    let navigateTo = document.querySelector(load);
+    let navigateIndex = current.findIndex(function(element) {
+        return element === load;
+    });
+
+    current.splice(navigateIndex, 1);
+
+    for (let i=0; i < current.length; i++) {
+        let currentPage = document.querySelector(current[i]);
+
+        currentPage.classList.add("pageOut");
+        currentPage.classList.remove("pageIn");
+
+        setTimeout(navigateCurrent, 500);
+
+        function navigateCurrent() {
+        currentPage.setAttribute("style", "display:none");
+        }
+    }
+
+    setTimeout(navigate, 500);
+
+    function navigate() {
+        navigateTo.setAttribute("style", "display:block");
+        navigateTo.classList.add("pageIn");
+        navigateTo.classList.remove("pageOut");
+    }
+
+    console.log(load + " from " + current);
+}
