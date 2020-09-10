adc6cda27d09487e9f8c4aa9af2c2f08e50a242f
Luis Santos <ls@MacBook-Pro-LS.local>
2020-09-10 10:54:34 -0600

    Prueba PWA

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-var cacheName = "pwa";
+var cacheName = "ProyectoPOO";
 var filesToCache = [
-  "/ProyectoPOO/index.html",
+  "/index.html",
 ];
 
 /* Start the service worker and cache all of the app's content */
