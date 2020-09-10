92b89a6b0130e023ccfb5ccc9ee8b35572b39940
Gabriela Paola Contreras Guerra <Contreras@users.noreply.github.com>
2020-09-09 19:00:09 -0600

    Update Validar.js

--- js/Validar.js
+++ js/Validar.js
@@ -29,6 +29,7 @@ class Validar {
     constructor(materia){
 
         var preguntas = []; // Arraylist que guarda las preguntas temporalmente
+        var correcta=[]; //ArrayList que guarda las respuestas temporalmente  
 
         // Si materia es 1 
         if (materia == 1){
@@ -49,7 +50,7 @@ class Validar {
         // Se crean 5 preguntas de la materia correspondiente 
         for (var i = 0; i < 5; i++){
 
-            preguntas.push(new Pregunta(materia, i))
+            preguntas.push(new Pregunta(materia, i)) // se crea una nueva instancia y se guarda en el array preguntas.
         }
 
         this.preguntas = preguntas; // Se asigna un array llena con las 5 preguntas de la materia al atributo preguntas
@@ -74,7 +75,20 @@ class Validar {
         }
 
         return ret; // Se devuelve ret
-    };
+    }
+    
+    // Método para corroborar que la respuesta seleccionada este correcta 
+    corroborar(correcta){
+        //Se crea un ciclo para reccorer el array de respuestas
+        for (var i = 0; i < correcta.length; i++) {
+            // Se crea la condicion para comparar la respuesta dada por el usuario en el array de respuestas basandose en el index 
+            if (preguntas[Pregunta].getRespuesta() == correcta[i]) {
+                this.cont_respuestas++; // se le suma 1 al contador de respuestas
+                this.cont_preguntas++; // se le suma 1 al contador de preguntas
+            } else 
+                this.cont_preguntas++; // se le suma 1 al contador de respuestas
+            }
+    }
 
     /*  Getters */  
 
