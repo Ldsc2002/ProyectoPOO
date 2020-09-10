1c634abe51021ec471a0b8ea572633b341e0e25a
Luis Santos <ls@MacBook-Pro-LS.local>
2020-09-10 10:16:35 -0600

    Changes in service worker

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var cacheName = 'hello-pwa';
+var cacheName = "pwa";
 var filesToCache = [
   "/",
   "index.html",
@@ -11,7 +11,7 @@ var filesToCache = [
 ];
 
 /* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function(e) {
+self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
       return cache.addAll(filesToCache);
@@ -20,7 +20,7 @@ self.addEventListener('install', function(e) {
 });
 
 /* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
+self.addEventListener("fetch", function(e) {
   e.respondWith(
     caches.match(e.request).then(function(response) {
       return response || fetch(e.request);
