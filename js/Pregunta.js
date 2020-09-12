71abfffd16d4d334b4a295fa91ac6d47fff0295b
Luis Santos <ls@MacBook-Pro-LS.local>
2020-09-11 22:20:18 -0600

    Revert "Encapsulación - Atributos privados"
    This reverts commit 95d4769bdb654bc3eb12f8b0d7088f83bd7567b5.

--- js/Pregunta.js
+++ js/Pregunta.js
@@ -22,10 +22,6 @@ class Pregunta{
     // Array pregunta: Guarda Pregunta textual
     // Array incorrect: Array de respuestas erróneas
 
-    #respuestas
-    #pregunta
-    #incorrect
-
     /*  Métodos */
 
     // constructor
@@ -38,34 +34,34 @@ class Pregunta{
             var preguntas = ["4 - 2", "3 x 3", "(7 + 2) - 8", "5 x 1", "7 x 4"];
 
             // Se asigna respuesta correcta, materia y pregunta según los parámetros dados
-            this.#pregunta = ("Resuelva la siguiente operación matemática: " + preguntas[index]);
-            this.#respuesta = respuestas[index];
+            this.pregunta = ("Resuelva la siguiente operación matemática: " + preguntas[index]);
+            this.respuesta = respuestas[index];
             
 
             // Si el parámetro index es 0 (primera pregunta de 5)
             if (index == 0){
 
-                this.#incorrect = [3, 5, 6]; // Se asignan las respuestas incorrectas segun la pregunta
+                this.incorrect = [3, 5, 6]; // Se asignan las respuestas incorrectas segun la pregunta
 
             // Si el parámetro index es 1 (segunda pregunta de 5)
             } else if (index == 1){
                 
-                this.#incorrect = [6, 3, 5]; // Se asignan las respuestas incorrectas segun la pregunta
+                this.incorrect = [6, 3, 5]; // Se asignan las respuestas incorrectas segun la pregunta
 
             // Si el parámetro index es 2 (tercera pregunta de 5)
             } else if (index == 2){
 
-                this.#incorrect = [3, 9, 8]; // Se asignan las respuestas incorrectas segun la pregunta        
+                this.incorrect = [3, 9, 8]; // Se asignan las respuestas incorrectas segun la pregunta        
 
             // Si el parámetro index es 3 (cuarta pregunta de 5)
             } else if (index == 3){
 
-                this.#incorrect = [6, 7, 4]; // Se asignan las respuestas incorrectas segun la pregunta
+                this.incorrect = [6, 7, 4]; // Se asignan las respuestas incorrectas segun la pregunta
                 
             // Si el parámetro index es 4 (quinta pregunta de 5)
             } else {
 
-                this.#incorrect = [20, 12, 14]; // Se asignan las respuestas incorrectas segun la pregunta
+                this.incorrect = [20, 12, 14]; // Se asignan las respuestas incorrectas segun la pregunta
             }
 
         } else if (materia == 2){
@@ -75,33 +71,33 @@ class Pregunta{
             var respuestas = ["seda", "rapido", "vuela", "bonita", "lejos"];
 
             // Se asigna respuesta correcta, materia y pregunta según los parámetros dados
-            this.#materia = "Lenguaje";
-            this.#pregunta = ("Complete la siguiente oración\n" + preguntas[index]);
-            this.#respuesta = respuestas[index];
+            this.materia = "Lenguaje";
+            this.pregunta = ("Complete la siguiente oración\n" + preguntas[index]);
+            this.respuesta = respuestas[index];
 
             // Si el parámetro index es 0 (primera pregunta de 5)
             if (index == 0){
 
-                this.#incorrect = ["lana", "risa", "gusanos"]; // Se asignan las respuestas incorrectas segun la pregunta
+                this.incorrect = ["lana", "risa", "gusanos"]; // Se asignan las respuestas incorrectas segun la pregunta
 
             // Si el parámetro index es 1 (segunda pregunta de 5)
             } else if (index == 1){
                 
-                this.#incorrect = ["niño", "corre", "bien"]; // Se asignan las respuestas incorrectas segun la pregunta
+                this.incorrect = ["niño", "corre", "bien"]; // Se asignan las respuestas incorrectas segun la pregunta
 
             // Si el parámetro index es 2 (tercera pregunta de 5)
             } else if (index == 2){
 
-                this.#incorrect = ["es", "corre", "flota"]; // Se asignan las respuestas incorrectas segun la pregunta          
+                this.incorrect = ["es", "corre", "flota"]; // Se asignan las respuestas incorrectas segun la pregunta          
 
             // Si el parámetro index es 3 (cuarta pregunta de 5)
             } else if (index == 3){
 
-                this.#incorrect = ["casa", "alegre", "verde"]; // Se asignan las respuestas incorrectas segun la pregunta
+                this.incorrect = ["casa", "alegre", "verde"]; // Se asignan las respuestas incorrectas segun la pregunta
                 
             } else {
 
-                this.#incorrect = ["cerca", "sonriente", "redondo"]; // Se asignan las respuestas incorrectas segun la pregunta
+                this.incorrect = ["cerca", "sonriente", "redondo"]; // Se asignan las respuestas incorrectas segun la pregunta
             }
 
         } else {
@@ -111,34 +107,34 @@ class Pregunta{
             var preguntas = ["¿Con que sentido persivimos los sabores?", "Complete la siguiente oración\nEl sol es una _____ muy grande", "La gallina pone huevos\nentonces esun animal: ", "Si un animal como plantas, entonces es: ", "El perro es un animal: "];
 
             // Se asigna respuesta correcta, materia y pregunta según los parámetros dados
-            this.#materia = "Ciencias Naturales";
-            this.#pregunta = (preguntas[index]);
-            this.#respuesta = respuestas[index];
+            this.materia = "Ciencias Naturales";
+            this.pregunta = (preguntas[index]);
+            this.respuesta = respuestas[index];
 
             // Si el parámetro index es 0 (primera pregunta de 5)
             if (index == 0){
 
-                this.#incorrect = ["vista", "olfato", "tacto"]; // Se asignan las respuestas incorrectas segun la pregunta
+                this.incorrect = ["vista", "olfato", "tacto"]; // Se asignan las respuestas incorrectas segun la pregunta
 
             // Si el parámetro index es 1 (segunda pregunta de 5)
             } else if (index == 1){
                 
-                this.#incorrect = ["galaxia", "luz", "constelación"]; // Se asignan las respuestas incorrectas segun la pregunta
+                this.incorrect = ["galaxia", "luz", "constelación"]; // Se asignan las respuestas incorrectas segun la pregunta
 
             // Si el parámetro index es 2 (tercera pregunta de 5)
             } else if (index == 2){
 
-                this.#incorrect = ["ave", "majestuoso", "vivíparo"]; // Se asignan las respuestas incorrectas segun la pregunta           
+                this.incorrect = ["ave", "majestuoso", "vivíparo"]; // Se asignan las respuestas incorrectas segun la pregunta           
 
             // Si el parámetro index es 3 (cuarta pregunta de 5)
             } else if (index == 3){
 
-                this.#incorrect = ["carnívoro", "saludable", "fuerte"]; // Se asignan las respuestas incorrectas segun la pregunta
+                this.incorrect = ["carnívoro", "saludable", "fuerte"]; // Se asignan las respuestas incorrectas segun la pregunta
                 
             // Si el parámetro index es 4 (quinta pregunta de 5)
             } else {
 
-                this.#incorrect = ["mamífero", "ovíparo", "herbívoro"]; // Se asignan las respuestas incorrectas segun la pregunta
+                this.incorrect = ["mamífero", "ovíparo", "herbívoro"]; // Se asignan las respuestas incorrectas segun la pregunta
             } 
         }
     }
@@ -147,21 +143,21 @@ class Pregunta{
 
    // materia
    getMateria() {
-       return this.#materia;
+       return this.materia;
    }
 
    // respuesta
    getRespuesta(){
-       return this.#respuesta;
+       return this.respuesta;
    }
 
    // pregunta
    getPregunta(){
-       return this.#pregunta;
+       return this.pregunta;
    }
 
    // incorrect
    getIncorrect(){
-       return this.#incorrect;
+       return this.incorrect;
    }
 }
--- js/Validar.js
+++ js/Validar.js
@@ -23,10 +23,6 @@ class Validar {
     // int cont_preguntas: Lleva el conteo de las preguntas que se le hacen al usuario.
     // int cont_respuestas: int - lleva el conteo de respuestas correctas que da el usuario.
 
-    #materia
-    #preguntas
-    #cont_preguntas
-
     /*  Métodos */
 
     // constructor
@@ -37,17 +33,17 @@ class Validar {
         // Si materia es 1 
         if (materia == 1){
 
-            this.#materia = "Matemática"; // asiganar materia matemática
+            this.materia = "Matemática"; // asiganar materia matemática
 
         // Si materia es 2
         } else if (materia == 2){
 
-            this.#materia = "Lenguaje"; // asiganar materia Lenguaje
+            this.materia = "Lenguaje"; // asiganar materia Lenguaje
 
         //Si materia es 3
         } else if (materia == 3){
 
-            this.#materia = "Ciencias Naturales"; // asiganar materia ciencias naturales
+            this.materia = "Ciencias Naturales"; // asiganar materia ciencias naturales
         }
 
         // Se crean 5 preguntas de la materia correspondiente 
@@ -56,8 +52,8 @@ class Validar {
             preguntas.push(new Pregunta(materia, i)) // se crea una nueva instancia y se guarda en el array preguntas.
         }
 
-        this.#preguntas = preguntas; // Se asigna un array llena con las 5 preguntas de la materia al atributo preguntas
-        this.#cont_preguntas = preguntas.length; // Se inicia en 0 el contador de preguntas
+        this.preguntas = preguntas; // Se asigna un array llena con las 5 preguntas de la materia al atributo preguntas
+        this.cont_preguntas = preguntas.length; // Se inicia en 0 el contador de preguntas
 
     }
 
@@ -67,13 +63,13 @@ class Validar {
         var pregunta = preg - 1; // Se le resta 1 al parámetro para que esté acorde a la indentación de los arrays
         var ret = []; // array que guardará el retorno
 
-        ret.push(this.#preguntas[pregunta].getPregunta()); // Se añade la respuesta de la pregunta corrspondiente a ret
-        ret.push(this.#preguntas[pregunta].getRespuesta()); // Se añade la respuesta de la pregunta correspondiente a ret
+        ret.push(this.preguntas[pregunta].getPregunta()); // Se añade la respuesta de la pregunta corrspondiente a ret
+        ret.push(this.preguntas[pregunta].getRespuesta()); // Se añade la respuesta de la pregunta correspondiente a ret
 
         // Se añaden las 3 respuestas incorrectas de la pregunta correspondiente a ret
         for (var i = 0; i < 3; i++){
 
-            ret.push(this.#preguntas[pregunta].getIncorrect()[i]);
+            ret.push(this.preguntas[pregunta].getIncorrect()[i]);
         }
 
         return ret; // Se devuelve ret
@@ -85,10 +81,10 @@ class Validar {
         for (var i = 0; i < correcta.length; i++) {
             // Se crea la condicion para comparar la respuesta dada por el usuario en el array de respuestas basandose en el index 
             if (preguntas[Pregunta].getRespuesta() == correcta[i]) {
-                this.#cont_respuestas++; // se le suma 1 al contador de respuestas
-                this.#cont_preguntas++; // se le suma 1 al contador de preguntas
+                this.cont_respuestas++; // se le suma 1 al contador de respuestas
+                this.cont_preguntas++; // se le suma 1 al contador de preguntas
             } else 
-                this.#cont_preguntas++; // se le suma 1 al contador de respuestas
+                this.cont_preguntas++; // se le suma 1 al contador de respuestas
             }
     }
 
@@ -96,11 +92,11 @@ class Validar {
 
     // materia
     getMateria() {
-        return this.#materia;
+        return this.materia;
     }
 
     // cont_preguntas
     getCont_preguntas() {
-        return this.#cont_preguntas;
+        return this.cont_preguntas;
     }
 }
