95d4769bdb654bc3eb12f8b0d7088f83bd7567b5
Luis Santos <ls@MacBook-Pro-LS.local>
2020-09-11 22:05:55 -0600

    Encapsulación - Atributos privados

--- js/Controlador.js
+++ js/Controlador.js
@@ -16,6 +16,11 @@ document.addEventListener('DOMContentLoaded', function() { //Espera a que la pá
 });
 
 class Controlador { //Clase de controlador
+    #preguntas
+    #val
+    #count
+    #respuesta
+
     constructor() {
         var elems = document.querySelectorAll('.sidenav'); //Navegación movil
         var instances = M.Sidenav.init(elems);
@@ -31,30 +36,30 @@ class Controlador { //Clase de controlador
 
     asignarMateria(materia) { //Constructor de la clase
         //Atributos de la clase
-        this.val = new Validar(materia); //Nueva instancia de validar
-        this.preguntas = []; //Preguntas disponibles
-        this.count = 0; //Cuenta de preguntas hechas por el usuario
+        this.#val = new Validar(materia); //Nueva instancia de validar
+        this.#preguntas = []; //Preguntas disponibles
+        this.#count = 0; //Cuenta de preguntas hechas por el usuario
 
-        for (let i = 1; i < (this.val.getCont_preguntas() + 1); i++) { //Por cada una de las preguntas
-            let temp = this.val.preguntaList(i); //Obtiene la pregunta y la copia a temp
-            this.preguntas.push(temp); //Añade la pregunta a this.preguntas
+        for (let i = 1; i < (this.#val.getCont_preguntas() + 1); i++) { //Por cada una de las preguntas
+            let temp = this.#val.preguntaList(i); //Obtiene la pregunta y la copia a temp
+            this.#preguntas.push(temp); //Añade la pregunta a this.preguntas
         }
 
         this.newPregunta();
     }
 
     newPregunta() { //Cambia los valores del HTML con los de la pregunta
-        if ((this.count + 1) > this.preguntas.length) { //Si ya se respondieron todas las preguntas
+        if ((this.#count + 1) > this.#preguntas.length) { //Si ya se respondieron todas las preguntas
             M.toast({html: "Has completado todas las preguntas!"})
         } else{ //Si aun hay preguntas
-            let x = this.count; //Copia el valor the this.count
-            document.getElementById("preguntaOut").innerHTML = this.preguntas[x][0]; //Obtiene la pregunta
-            document.getElementById("materiaOut").innerHTML = this.val.getMateria(); //Obtiene la materia
+            let x = this.#count; //Copia el valor the this.count
+            document.getElementById("preguntaOut").innerHTML = this.#preguntas[x][0]; //Obtiene la pregunta
+            document.getElementById("materiaOut").innerHTML = this.#val.getMateria(); //Obtiene la materia
 
-            let temp = this.preguntas[x].slice(); //Copia la pregunta y sus respuestas al array
+            let temp = this.#preguntas[x].slice(); //Copia la pregunta y sus respuestas al array
             temp.shift(); //Borra el primer valor, que contiene la pregunta
 
-            this.mezclarRespuestas(temp); //Mezcla el array temp
+            this.#mezclarRespuestas(temp); //Mezcla el array temp
 
             //Asigna el texto y el valor de cada una de las respuestas a un elemento del HTML
             document.getElementById("Opcion1").innerHTML = temp[0];
@@ -66,12 +71,12 @@ class Controlador { //Clase de controlador
             document.getElementById("Opcion4").innerHTML = temp[3];
             document.getElementById("Radio4").value = temp[3];
                 
-            this.respuesta = this.preguntas[x][1]; //Asigna la respuesta de la pregunta a this.pregunta
-            this.count++; //Suma uno a count
+            this.#respuesta = this.#preguntas[x][1]; //Asigna la respuesta de la pregunta a this.pregunta
+            this.#count++; //Suma uno a count
         }
        
     }
-    mezclarRespuestas(array) { //Mezcla un array para randomizar las posiciones de las respuestas
+    #mezclarRespuestas(array) { //Mezcla un array para randomizar las posiciones de las respuestas
         var currentIndex = array.length, temporaryValue, randomIndex;
       
         // Mientras hay elementos que mezclar
@@ -95,7 +100,7 @@ class Controlador { //Clase de controlador
     
         for (let i = 0; i < form.length; i++) { //Verifica todos los elementos 
             if (form[i].checked) { //Revisa si es la opción seleccionada por el usuario
-                if (form[i].value == this.respuesta) { //Revisa si es la respuesta correcta
+                if (form[i].value == this.#respuesta) { //Revisa si es la respuesta correcta
                     M.toast({html: "Respuesta correcta!"}) 
                     this.newPregunta() //Pasa a la siguiente pregunta
                 } else { //Si la respuesta no es correcta
