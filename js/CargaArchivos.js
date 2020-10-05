63630917b5339b27b6c0afa432d3e28a52baff02
Luis Santos <ls@MacBook-Pro-LS.local>
2020-10-05 13:00:51 -0600

    Prueba con varios archivos

--- js/CargaArchivos.js
+++ js/CargaArchivos.js
@@ -1,7 +1,6 @@
 class CargaArchivos {
     constructor() {
         var xmlhttp;
-        let materia = 1;
         let preguntas;
 
         if (window.XMLHttpRequest) { // IE7+, Firefox, Chrome, Opera, Safari
@@ -19,35 +18,11 @@ class CargaArchivos {
                         console.log("Finished:", results);
                         preguntas = results.data;
 
-                        if (materia == 1){
-                            for (var i = 0; i < preguntas.length; i++) {
-                                let temp = preguntas[i][0]
-                                preguntas[i][0] = ("Resuelva la siguiente operación matemática: " + temp);
-                
-                            }
-                            sessionStorage.setItem("matematica", JSON.stringify(preguntas))
-                            materia++;
-
-                        } else if (materia == 2){
-                            for (var i = 0; i < preguntas.length; i++) {
-                                let temp = preguntas[i][0]
-                                preguntas[i][0] = ("Complete la siguiente oración: " + temp);
-                
-                            }
-                            sessionStorage.setItem("lenguaje", JSON.stringify(preguntas))
-                            materia++;
-
-                        } else if (materia == 3){
-                            for (var i = 0; i < preguntas.length; i++) {
-                                let temp = preguntas[i][0]
-                                preguntas[i][0] = ("Complete la siguiente oración: " + temp);
-                
-                            }
-                            sessionStorage.setItem("ciencias", JSON.stringify(preguntas))
-                            materia++;
-
-                        } 
-
+                        for (var i = 0; i < preguntas.length; i++) {
+                            let temp = preguntas[i][0]
+                            preguntas[i][0] = ("Resuelva la siguiente operación matemática: " + temp);
+                        }
+                        sessionStorage.setItem("matematica", JSON.stringify(preguntas))
                     }
                 });
             }
@@ -55,10 +30,60 @@ class CargaArchivos {
 
         xmlhttp.open("GET", "db/matematica.csv", true);
         xmlhttp.send();
-            
+
+        if (window.XMLHttpRequest) { // IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp = new XMLHttpRequest();
+        } else { // IE6, IE5
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                var text = xmlhttp.responseText;
+        
+                Papa.parse(text, {
+                    complete: function(results) {
+                        console.log("Finished:", results);
+                        preguntas = results.data;
+
+                        for (var i = 0; i < preguntas.length; i++) {
+                            let temp = preguntas[i][0]
+                            preguntas[i][0] = ("Complete la siguiente oración: " + temp);
+                        }
+                        sessionStorage.setItem("lenguaje", JSON.stringify(preguntas))
+                    }
+                });
+            }
+        }
+
         xmlhttp.open("GET", "db/lenguaje.csv", true);
         xmlhttp.send();
 
+        if (window.XMLHttpRequest) { // IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp = new XMLHttpRequest();
+        } else { // IE6, IE5
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                var text = xmlhttp.responseText;
+        
+                Papa.parse(text, {
+                    complete: function(results) {
+                        console.log("Finished:", results);
+                        preguntas = results.data;
+
+                        for (var i = 0; i < preguntas.length; i++) {
+                            let temp = preguntas[i][0]
+                            preguntas[i][0] = (temp);
+                        }
+                        sessionStorage.setItem("ciencias", JSON.stringify(preguntas))
+                    }
+                });
+            }
+        }
+        
         xmlhttp.open("GET", "db/ciencias.csv", true);
         xmlhttp.send();
 
