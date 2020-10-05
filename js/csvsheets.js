aa0efbe5ecedcdd997d21dfd743656d790e49bbd
Pepe Miranda <mirjoseduardo@gmail.com>
2020-10-04 21:36:05 -0600

    Prueba lectura Google Sheets

---
+++ js/csvsheets.js
@@ -0,0 +1,36 @@
+:unction parseCSV(text) {
+  // Obtenemos las lineas del texto
+  let lines = text.replace(/\r/g, '').split('\n');
+  return lines.map(line => {
+    // Por cada linea obtenemos los valores
+    let values = line.split(',');
+    return values;
+  });
+}
+
+function reverseMatrix(matrix){
+  let output = [];
+  // Por cada fila
+  matrix.forEach((values, row) => {
+    // Vemos los valores y su posicion
+    values.forEach((value, col) => {
+      // Si la posición aún no fue creada
+      if (output[col] === undefined) output[col] = [];
+      output[col][row] = value;
+    });
+  });
+  return output;
+}
+
+function readFile('https://docs.google.com/spreadsheets/d/e/2PACX-1vRi0TErB19VVDtMywTl3CjNweMuABO7Ot25_x0C2MMqgfHPohJerAkBGAf6aS0T94xZ6-9WDLLcmTJ1/pub?output=csv') {
+  let file = evt.target.files[0];
+  let reader = new FileReader();
+  reader.onload = (e) => {
+    // Cuando el archivo se terminó de cargar
+    let lines = parseCSV(e.target.result);
+    let output = reverseMatrix(lines);
+    console.log(output);
+  };
+  // Leemos el contenido del archivo seleccionado
+  reader.readAsBinaryString(file);
+}
