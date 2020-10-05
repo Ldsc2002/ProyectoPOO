86d709cfa0ce0215de02850a8d84acf3f22118b4
Luis Santos <ls@MacBook-Pro-LS.local>
2020-10-04 20:51:12 -0600

    Prueba con CSV local

--- js/PreguntaPROTO.js
+++ js/PreguntaPROTO.js
@@ -1,8 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-    Papa.parse("https://docs.google.com/spreadsheets/d/e/2PACX-1vTY9aEURaOJOJd7ZF3g0yE-nbA8HO6rfPjdKJHLp8JGrmJSiz44weM_BtZizodIjhdwtYFpyeiD75C1/pub?output=csv", {
-        download: true,
+    Papa.parse("../db/matematica.csv", {
         complete: function(results) {
-            console.log(results);
+            console.log("Finished:", results.data);
         }
     });
 });
