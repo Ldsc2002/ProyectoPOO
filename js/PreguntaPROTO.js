9e10ece9cd4e84f17c897f2d125a1a66102d8eff
Luis Santos <ls@MacBook-Pro-LS.local>
2020-10-04 23:09:26 -0600

    Prueba de carga de archivo

--- js/PreguntaPROTO.js
+++ js/PreguntaPROTO.js
@@ -1,6 +1,26 @@
 var file = new File(["matematica"], "../db/matematica.csv")
 
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("text/csv");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+}
+
+
+
 document.addEventListener('DOMContentLoaded', function() {
+    
+    readTextFile("../db/matematica.csv", function(text){
+        file = JSON.parse(text);
+        console.log(file);
+    });
+    
     Papa.parse(file, {
         complete: function(results) {
             console.log("Finished:", results);
