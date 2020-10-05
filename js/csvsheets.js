4431300113e36721453cf92a563c3e0c93802c89
Luis Santos <ls@MacBook-Pro-LS.local>
2020-10-04 23:22:58 -0600

    Prueba

--- js/csvsheets.js
+++ js/csvsheets.js
@@ -1,19 +1,20 @@
-var array = [];
 var xmlhttp;
+
 if (window.XMLHttpRequest) { // IE7+, Firefox, Chrome, Opera, Safari
     xmlhttp = new XMLHttpRequest();
 } else { // IE6, IE5
     xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
 }
+
 xmlhttp.onreadystatechange = function() {
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+    if (this.readyState == 4 && this.status == 200) {
         var text = xmlhttp.responseText;
+        console.log(text)
         // Now convert it into array using regex
         array = text.split(/\n|\r/g);
     }
 }
+
 xmlhttp.open("GET", "data.csv", true);
 xmlhttp.send();
-console.log(xmlhttp.responseText);
-console.log(array);
 
